67b4c71077add93046db69849946f3eb5a5d4a98
wiznova <srg.khalil@gmail.com>
2022-12-03 23:12:25 +0100

    Day 2: Added derives for Copy and Clone

--- 2_1_main.rs
+++ 2_1_main.rs
@@ -1,44 +1,62 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Choice {
-    Rock(u8),
-    Paper(u8),
-    Scissors(u8),
+    Rock,
+    Paper,
+    Scissors,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum State {
-    Lose(u8),
-    Tie(u8),
-    Win(u8),
+    Lose,
+    Tie,
+    Win,
 }
 
 fn to_choice(letter: char) -> Choice {
     match letter {
-        'A' => Choice::Rock(0),
-        'B' => Choice::Paper(0),
-        'C' => Choice::Scissors(0),
-        'X' => Choice::Rock(1),
-        'Y' => Choice::Paper(2),
-        'Z' => Choice::Scissors(3),
+        'A' => Choice::Rock,
+        'B' => Choice::Paper,
+        'C' => Choice::Scissors,
+        'X' => Choice::Rock,
+        'Y' => Choice::Paper,
+        'Z' => Choice::Scissors,
         _ => todo!(),
     }
 }
 
 fn to_state(opponent: Choice, me: Choice) -> State {
     match (me, opponent) {
-        (Choice::Rock(_), Choice::Paper(_)) => State::Lose(0),
-        (Choice::Paper(_), Choice::Scissors(_)) => State::Lose(0),
-        (Choice::Scissors(_), Choice::Rock(_)) => State::Lose(0),
-        (Choice::Rock(_), Choice::Rock(_)) => State::Tie(3),
-        (Choice::Paper(_), Choice::Paper(_)) => State::Tie(3),
-        (Choice::Scissors(_), Choice::Scissors(_)) => State::Tie(3),
-        (Choice::Paper(_), Choice::Rock(_)) => State::Win(6),
-        (Choice::Scissors(_), Choice::Paper(_)) => State::Win(6),
-        (Choice::Rock(_), Choice::Scissors(_)) => State::Win(6),
-        _ => todo!(),
+        (Choice::Rock, Choice::Paper) => State::Lose,
+        (Choice::Paper, Choice::Scissors) => State::Lose,
+        (Choice::Scissors, Choice::Rock) => State::Lose,
+        (Choice::Rock, Choice::Rock) => State::Tie,
+        (Choice::Paper, Choice::Paper) => State::Tie,
+        (Choice::Scissors, Choice::Scissors) => State::Tie,
+        (Choice::Paper, Choice::Rock) => State::Win,
+        (Choice::Scissors, Choice::Paper) => State::Win,
+        (Choice::Rock, Choice::Scissors) => State::Win,
     }
+}
+
+fn state_to_value(state: State) -> u8 {
+    match state {
+        State::Lose => 0,
+        State::Tie => 3,
+        State::Win => 6,
+    }
+}
+fn choice_to_value(choice: Choice) -> u8 {
+    match choice {
+        Choice::Rock => 1,
+        Choice::Paper => 2,
+        Choice::Scissors => 3,
+    }
+}
+
+fn calculate_score(my_choice: Choice, state: State) -> u8 {
+    choice_to_value(my_choice) + state_to_value(state)
 }
 
 fn main() {
@@ -47,14 +65,20 @@ fn main() {
     println!("In file {}", file_path);
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let split: Vec<&str> = contents.split("\n").collect();
+    // let acc = 0;
 
     for s in split {
-        break
-        // println!("{}", s);
+        if s == "" {
+            break
+        }
+        let char_vec: Vec<char> = s.chars().collect();
+        let opponent = to_choice(char_vec[0]);
+        let me = to_choice(char_vec[char_vec.len() - 1]);
+        let state = to_state(opponent, me);
+        println!("state-{} {:?}", calculate_score(me, state), state);
+        // acc += me.
     }
-    let opponent = Choice::Rock(0);
-    let me = Choice::Scissors(2);
-    println!("State: {:?}", to_state(opponent, me));
+    // println!("State: {:?}", to_state(opponent, me));
 
 
 }
