099a3aba733a0c95f2fdf62e91565240db93add9
wiznova <srg.khalil@gmail.com>
2022-12-04 13:26:05 +0100

    Day 4: Solve puzzle 2

---
+++ 4_2_main.rs
@@ -0,0 +1,57 @@
+use std::fs;
+
+#[derive(Debug, Copy, Clone)]
+struct Range {
+    start: i32,
+    end: i32,
+}
+
+impl Range {
+    pub fn encloses(&self, other: Range) -> bool {
+        if self.start <= other.start && self.end >= other.end {
+            true
+        } else {
+            false
+        }
+    }
+    pub fn overlaps(&self, other: Range) -> bool {
+        let overlaps_left: bool = self.start <= other.end && self.start >= other.start;
+        let overlaps_right: bool = other.start <= self.end && other.start >= self.start;
+        if  overlaps_left || overlaps_right {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn main() {
+    let file_path = "./4_input.txt";
+    println!("In file {}", file_path);
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let split: Vec<&str> = contents.split("\n").collect();
+    let mut acc: u32 = 0;
+
+    for s in split {
+        let ranges: Vec<&str> = s.split(",").collect();
+
+        let left_range_str: Vec<&str> = ranges[0].split("-").collect();
+        let left_range = Range {
+            start: left_range_str[0].parse::<i32>().unwrap(),
+            end: left_range_str[1].parse::<i32>().unwrap(),
+        };
+
+        let right_range_str: Vec<&str> = ranges[1].split("-").collect();
+        let right_range = Range {
+            start: right_range_str[0].parse::<i32>().unwrap(),
+            end: right_range_str[1].parse::<i32>().unwrap(),
+        };
+
+        if left_range.overlaps(right_range) {
+            acc += 1;
+        }
+        // println!("Line {}", s);
+    }
+
+    println!("Score: {}", acc);
+}
