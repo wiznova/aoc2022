04183aad3a8127574b88f45f7cdefa74717ef30c
wiznova <srg.khalil@gmail.com>
2022-12-06 19:20:34 +0100

    Day 6: Solve challenge 1;

---
+++ 6_1_main.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn count_unique(list: &[char]) -> usize {
+    let mut set = HashSet::new();
+    for &x in list {
+        set.insert(x);
+    }
+    set.len()
+}
+
+fn main() {
+    let file_path = "./6_input.txt";
+    println!("In file {}", file_path);
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    // let contents = "nppdvjthqldpwncqszvftbrmjlhg";
+    let window_size = 4;
+    // let split: Vec<&str> = contents.split("\n").collect();
+
+    for (i, window) in contents
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(window_size)
+        .enumerate()
+    {
+        // println!("{} {:?}", i, window);
+        if count_unique(window) == window_size {
+            println!("{} {:?}", i, window);
+            println!("idx = {}", (i + 3 + 1));
+            break
+        }
+    }
+}
