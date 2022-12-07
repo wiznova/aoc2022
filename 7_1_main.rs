3006912fac7c49014e0f19879e45c931c61f6a64
wiznova <srg.khalil@gmail.com>
2022-12-07 21:19:31 +0100

    Day 7: WIP, flush while figuring out the mutability borrow issue

--- 7_1_main.rs
+++ 7_1_main.rs
@@ -1,18 +1,28 @@
 use std::fs;
 
+pub trait MutChildren {
+    fn files_mut(&mut self) -> &mut Vec<File>;
+}
+
 #[derive(Debug, Copy, Clone)]
 struct File<'a> {
     name: &'a str,
     size: i32,
-    parent: &'a Folder<'a>,
+    parent: Option<&'a Folder<'a>>,
 }
 
 #[derive(Debug, Clone)]
 struct Folder<'a> {
     name: &'a str,
-    parent: Option<&'a Folder <'a>>,
-    folders: Vec<&'a Folder<'a>>,
-    files: Vec<&'a File<'a>>,
+    parent: Option<&'a Folder<'a>>,
+    folders: Vec<Folder<'a>>,
+    files: Vec<File<'a>>,
+}
+
+impl MutChildren for Folder<'_> {
+    fn files_mut(&mut self) -> &mut Vec<File> {
+        &mut self.files
+    }
 }
 
 fn is_numeric(s: &str) -> bool {
@@ -24,20 +34,38 @@ fn is_numeric(s: &str) -> bool {
     true
 }
 
+fn new_file<'a>(name: &'a str, size: i32, parent: &'a Folder) -> File<'a> {
+    File {
+        name: name,
+        size: size,
+        parent: Some(parent),
+    }
+}
+fn new_folder<'a>(name: &'a str, parent: &'a Folder) -> Folder<'a> {
+    Folder {
+        name: name,
+        folders: Vec::new(),
+        files: Vec::new(),
+        parent: Some(parent),
+    }
+}
+
 fn main() {
     let file_path = "./7_input.txt";
     println!("In file {}", file_path);
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     // let contents = "nppdvjthqldpwncqszvftbrmjlhg";
     let split: Vec<&str> = contents.split("\n").collect();
+    // let folders: Vec<Folder> = Vec::new();
 
     assert_eq!(split[0], "$ cd /");
-    let root = Folder{
+    let mut root = Folder {
         name: "/",
         folders: Vec::new(),
         files: Vec::new(),
-        parent: None
+        parent: None,
     };
+    let mut current_folder = &mut root;
 
     for s in &split[1..] {
         let s_split = s.split(" ");
@@ -47,12 +75,27 @@ fn main() {
         }
         // println!("{:?}", s_vec);
         match s_vec[..] {
-            ["$", command, arg1, ..] => println!("command: {} {}", command, arg1),
-            ["$", command, ..] => println!("command: {}", command),
-            ["dir", dir_name, ..] => println!("directory: {}", dir_name),
-            [size, filename, ..] if is_numeric(size) => println!("file: {}-{}", filename, size),
-            [] | [_] | [_, _, _, _, ..] => todo!(),
-            [&_, _] | [&_, _, _] => todo!()
+            ["$", "ls"] => println!("LS start"),
+            ["$", "cd", ".."] => {
+                println!("cd back");
+                current_folder = &mut current_folder.parent.unwrap();
+            }
+            ["$", "cd", path] => {
+                println!("cd into: {}", path);
+            }
+            ["dir", dir_name] => {
+                println!("dir: {}", dir_name);
+                current_folder.folders
+                .push(new_folder(dir_name, current_folder));
+            }
+            [size, filename] if is_numeric(size) => {
+                println!("file: {}-{}", filename, size);
+                current_folder
+                    .files_mut()
+                    .push(new_file(filename, size.parse::<i32>().unwrap(), current_folder));
+            }
+            [] | [_] => todo!(),
+            [&_, _] | [&_, _, _, ..] => todo!(),
         }
         // println!("Line: {}", s);
     }
