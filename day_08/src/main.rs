e982455650c86697419fa5e73fe72820514b7427
wiznova <srg.khalil@gmail.com>
2022-12-10 12:03:23 +0100

    Day 8: Solve challenge 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -63,11 +63,74 @@ impl Forest<'_> {
             || self.visible_left(row, col)
             || self.visible_right(row, col);
     }
+
+    fn count_left(&self, row: usize, col: usize) -> u64 {
+        let height = self.ch(row, col);
+        let mut co = 0;
+        if col == 0 {
+            return 0;
+        }
+        for i in (0..col).rev() {
+            co += 1;
+            if self.ch(row, i) >= height {
+                break;
+            }
+        }
+        co
+    }
+    fn count_right(&self, row: usize, col: usize) -> u64 {
+        let height = self.ch(row, col);
+        let mut co = 0;
+        if col == self.max_i {
+            return 0;
+        }
+        for i in col + 1..self.max_i + 1 {
+            co += 1;
+            if self.ch(row, i) >= height {
+                break;
+            }
+        }
+        co
+    }
+    fn count_down(&self, row: usize, col: usize) -> u64 {
+        let height = self.ch(row, col);
+        let mut co = 0;
+        if row == self.max_i {
+            return 0;
+        }
+        for i in row + 1..self.max_i + 1 {
+            co += 1;
+            if self.ch(i, col) >= height {
+                break;
+            }
+        }
+        co
+    }
+    fn count_up(&self, row: usize, col: usize) -> u64 {
+        let height = self.ch(row, col);
+        let mut co = 0;
+        if row == 0 {
+            return 0;
+        }
+        for i in (0..row).rev() {
+            co += 1;
+            if self.ch(i, col) >= height {
+                break;
+            }
+        }
+        co
+    }
+    fn scenic_score(&self, row: usize, col: usize) -> u64 {
+        return self.count_down(row, col)
+            * self.count_up(row, col)
+            * self.count_left(row, col)
+            * self.count_right(row, col);
+    }
 }
 
 fn main() {
     let file_path = "./input.txt";
-    // let test_file_path = "./test_input.txt";
+    // let file_path = "./test_input.txt";
     println!("In file {}", file_path);
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let split: Vec<&str> = contents.split("\n").collect();
@@ -78,16 +141,34 @@ fn main() {
         max_i: s_len - 1,
     };
 
-    let mut total = 0u64;
-    
-    for row in 0..s_len {
-        for col in 0..s_len {
-            if f.visible(row, col) {
-                total += 1;
+    // challenge 1
+    {
+        let mut total = 0u64;
+
+        for row in 0..s_len {
+            for col in 0..s_len {
+                if f.visible(row, col) {
+                    total += 1;
+                }
             }
         }
+        println!("ch1: {total}");
     }
-    println!("{total}");
+
+    // challenge 2
+    {
+        let mut max_score = 0u64;
+        for row in 0..s_len {
+            for col in 0..s_len {
+                let score =f.scenic_score(row, col); 
+                if score  > max_score {
+                    max_score = score;
+                }
+            }
+        }
+        println!("ch2: {max_score}");
+    }
+
     // println!("{}", f.visible_right(0, 4));
     // println!("{}", f.visible_left(f.max_i, f.max_i));
     // println!("{}", f.visible_up(1, f.max_i));
