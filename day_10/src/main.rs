3ce30358b368a9d6db04f3c8e854ce9d006241cb
wiznova <srg.khalil@gmail.com>
2022-12-19 23:39:49 +0100

    Day 10: Solve challenge 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,20 +1,40 @@
 use std::fs;
 
 static TICKS: [i64; 6] = [20, 60, 100, 140, 180, 220];
+static WIDTH: i64 = 40;
 
 struct CPU {
     _co: i64,
     x: i64,
-    _sum: i64
+    _sum: i64,
 }
 
 impl CPU {
     fn tick(&mut self) {
         self._co += 1;
+        self.draw_pixel();
         if TICKS.contains(&self._co) {
-            let strength = self._co * self.x;
-            println!("tick {}: {}", self._co, strength);
-            self._sum += strength;
+            self._sum += self._co * self.x;
+        }
+    }
+    fn draw_pixel(&self) {
+        let pos = self._co % WIDTH;
+        let sprite_pos = if self.x >= 0 {
+            self.x % WIDTH
+        } else {
+            WIDTH + self.x
+        };
+        // println!("cycle {}: {pos} {sprite_pos}", self._co);
+
+        let diff = pos - sprite_pos;
+        if diff >= 0 && diff <= 2 {
+            print!("#");
+        } else {
+            print!(".");
+        }
+
+        if pos == 0 {
+            print!("\n");
         }
     }
 }
@@ -27,7 +47,11 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let split: Vec<&str> = contents.split("\n").collect();
 
-    let mut cpu = CPU{_co: 0, x: 1, _sum: 0};
+    let mut cpu = CPU {
+        _co: 0,
+        x: 1,
+        _sum: 0,
+    };
 
     for s in &split {
         // start of the cycle
@@ -38,7 +62,7 @@ fn main() {
         let instr_split: Vec<&str> = s.split(" ").collect();
         let instr = instr_split[0];
         let param = instr_split[1].parse::<i64>().unwrap();
-        
+
         // second cycle
         cpu.tick();
 
